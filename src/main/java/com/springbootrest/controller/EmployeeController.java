0cc26c819442a097f63d6ccd05ab50dad65778e3
Ishan Ayesha <ishanayesha1@gmail.com>
2017-11-19 13:33:44 +0530

    changes

--- src/main/java/com/springbootrest/controller/EmployeeController.java
+++ src/main/java/com/springbootrest/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.springbootrest.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,11 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootrest.model.Employee;
-import com.springbootrest.model.Role;
 import com.springbootrest.service.EmployeeService;
 import com.springbootrest.service.RoleSerivce;
 
